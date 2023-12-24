760ee7dd04ac84a2b49147e4bb6e15efd1550b3f
Jaidev <jaidevyadav720@gmail.com>
2023-12-24 22:59:01 +0530

    todo app completed and blog app setup completed

---
+++ blogappwrite/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import config from "./config/config";
+function App() {
+  console.log(config.appwriteUrl)
+  return (
+    <>
+      <h1> appwrite blog</h1>
+    </>
+  );
+}
+
+export default App;
